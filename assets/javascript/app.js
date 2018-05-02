8ec937774ff2d8f5e692ca4a640784c29faef352
mleftakes <mleftakes@gmail.com>
2018-05-01 21:13:11 -0500

    Added initial html with external links to CSS, JS files. Added Firebase config to JS file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPH6H536ccjgr9M48YunIT1tkMaYDJaUo",
+    authDomain: "ihavearrived-e9123.firebaseapp.com",
+    databaseURL: "https://ihavearrived-e9123.firebaseio.com",
+    projectId: "ihavearrived-e9123",
+    storageBucket: "ihavearrived-e9123.appspot.com",
+    messagingSenderId: "393004989891"
+  };
+  firebase.initializeApp(config);
